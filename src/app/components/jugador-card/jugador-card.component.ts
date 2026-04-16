e82c08553e3eb74a453de7e0d66c2d383f141b82
Pau Ojeda Gallego <pojeda@elpuig.xeill.net>
2026-04-17 00:01:05 +0200

    Commit provisional

--- src/app/components/jugador-card/jugador-card.component.ts
+++ src/app/components/jugador-card/jugador-card.component.ts
@@ -1,11 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { Jugador } from 'src/app/services/jugadores.service';
+import { IonCard } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-jugador-card',
   templateUrl: './jugador-card.component.html',
-  styleUrl: './jugador-card.component.scss'
+  styleUrl: './jugador-card.component.scss',
+  imports: [IonCard]
 })
 export class PruebaComponent {
     @Input() jugador!: Jugador;
+
+    splitName(s: string) : string {
+      return s.split(' ')[0];
+    }
 }
--- src/app/views/equipo/equipo.page.ts
+++ src/app/views/equipo/equipo.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonInput, IonButton } from '@ionic/angular/standalone';
 import { Jugador, JugadoresService } from '../../services/jugadores.service';
 import { firstValueFrom } from 'rxjs';
 import { PruebaComponent } from "src/app/components/jugador-card/jugador-card.component";
@@ -11,7 +11,7 @@ import { PruebaComponent } from "src/app/components/jugador-card/jugador-card.co
   templateUrl: './equipo.page.html',
   styleUrls: ['./equipo.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, PruebaComponent, IonGrid, IonRow, IonCol]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, PruebaComponent, IonItem, IonInput, IonButton]
 })
 export class EquipoPage implements OnInit {
 
@@ -22,13 +22,11 @@ export class EquipoPage implements OnInit {
   async ngOnInit() {
     try {
       const response : Jugador[] = await firstValueFrom(this.jugadores.getJugadores());
-      console.log("Response ", response)
       for (let i = 0 ; i < response.length; i++) {  
         if (response[i].teamId === null) {
           this.jugadoresSinEquipo.push(response[i]);
         }
       }
-      console.log("Jugadores: ", this.jugadoresSinEquipo)
     } catch (error) {
       console.error("Ha ocurrido un error")
     }
